694d04d7e7ad157864628003458a24d6a32f6fac
Yunseo-Han <57244943+Yunseo-Han@users.noreply.github.com>
2020-06-21 21:40:01 -0700

    two new playable levels

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config params
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xVelocity = 2f;
+    [SerializeField] float yVelocity = 15f;
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, yVelocity);
+        }
+    }
+
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
