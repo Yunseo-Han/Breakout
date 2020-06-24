6d839a13c16acaecd0f2460d669480f692f503b2
Yunseo-Han <57244943+Yunseo-Han@users.noreply.github.com>
2020-06-23 22:11:03 -0700

    new sprites, block counters, new sounds

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        DeestroyBlock();
     }
 
+    private void DeestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //parameters
+    [SerializeField] int breakableBlocks;
+
+    // cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks() {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed() {
+        breakableBlocks--;
+        if (breakableBlocks <= 0) {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+
+}
